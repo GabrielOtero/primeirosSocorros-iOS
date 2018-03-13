0cfdcd4337a13e98110de878d299b30a1a682096
Gabriel Otero <otr.gabriel@gmail.com>
2018-03-13 20:10:32 -0300

    first commit

---
+++ Primeiros Socorros/Primeiros Socorros/MenuViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RootViewController.swift
+//  Primeiros Socorros
+//
+//  Created by Gabriel Otero on 13/03/18.
+//  Copyright © 2018 Gabriel Otero. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    var pageViewController: UIPageViewController?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+
