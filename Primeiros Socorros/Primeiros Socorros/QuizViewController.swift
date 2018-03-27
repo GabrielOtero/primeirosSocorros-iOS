f982006d5c78dfa08bbce81f2d35c82b17945696
Gabriel Otero <otr.gabriel@gmail.com>
2018-03-27 01:26:27 -0300

    q2

--- Primeiros Socorros/Primeiros Socorros/QuizViewController.swift
+++ Primeiros Socorros/Primeiros Socorros/QuizViewController.swift
@@ -39,6 +39,12 @@ class QuizPageViewController: UIViewController {
     @IBOutlet weak var q1opt3radio: DLRadioButton!
     @IBOutlet weak var q1opt4radio: DLRadioButton!
     
+    @IBOutlet weak var q2opt1radio: DLRadioButton!
+    @IBOutlet weak var q2opt2radio: DLRadioButton!
+    @IBOutlet weak var q2opt3radio: DLRadioButton!
+    @IBOutlet weak var q2opt4radio: DLRadioButton!
+    
+    
     @IBOutlet weak var q1Feedback: UILabel!
     
     
@@ -84,34 +90,69 @@ class QuizPageViewController: UIViewController {
         
     }
     
+    fileprivate func handleQ1() {
+        // UI updates on the main thread
+        
+        var q1SelectedOpt = 0
+        
+        if(self.q1opt1radio.isSelected){
+            q1SelectedOpt = 1
+        }
+        if(self.q1opt2radio.isSelected){
+            q1SelectedOpt = 2
+        }
+        if(self.q1opt3radio.isSelected){
+            q1SelectedOpt = 3
+        }
+        if(self.q1opt4radio.isSelected){
+            q1SelectedOpt = 4
+        }
+        
+        self.q1Feedback.isHidden = false
+        self.q1Feedback.transform = CGAffineTransform(rotationAngle: CGFloat.pi / -10)
+        if(q1SelectedOpt == LanguageManager.shared.currentLanguage.q1CorrectAnswer){
+            self.q1Feedback.text = LanguageManager.shared.currentLanguage.correctAnswer
+            self.q1Feedback.textColor =  UIColor.green
+        }else{
+            self.q1Feedback.text = LanguageManager.shared.currentLanguage.incorrectAnswer
+            self.q1Feedback.textColor =  UIColor.red
+        }
+    }
+    
+    fileprivate func handleQ2() {
+        // UI updates on the main thread
+        
+        var q2SelectedOpt = 0
+        
+        if(self.q2opt1radio.isSelected){
+            q2SelectedOpt = 1
+        }
+        if(self.q2opt2radio.isSelected){
+            q2SelectedOpt = 2
+        }
+        if(self.q2opt3radio.isSelected){
+            q2SelectedOpt = 3
+        }
+        if(self.q2opt4radio.isSelected){
+            q2SelectedOpt = 4
+        }
+        
+        self.q1Feedback.isHidden = false
+        self.q1Feedback.transform = CGAffineTransform(rotationAngle: CGFloat.pi / -10)
+        if(q2SelectedOpt == LanguageManager.shared.currentLanguage.q2CorrectAnswer){
+            self.q1Feedback.text = LanguageManager.shared.currentLanguage.correctAnswer
+            self.q1Feedback.textColor =  UIColor.green
+        }else{
+            self.q1Feedback.text = LanguageManager.shared.currentLanguage.incorrectAnswer
+            self.q1Feedback.textColor =  UIColor.red
+        }
+    }
+    
     @IBAction func sendClick(_ sender: UIButton) {
         UIView.animate(withDuration: 0.25, animations: { () -> Void in
-            DispatchQueue.main.async {  // UI updates on the main thread
-                
-                var q1SelectedOpt = 0
-            
-                if(self.q1opt1radio.isSelected){
-                    q1SelectedOpt = 1
-                }
-                if(self.q1opt2radio.isSelected){
-                    q1SelectedOpt = 2
-                }
-                if(self.q1opt3radio.isSelected){
-                    q1SelectedOpt = 3
-                }
-                if(self.q1opt4radio.isSelected){
-                    q1SelectedOpt = 4
-                }
-                
-                self.q1Feedback.isHidden = false
-                self.q1Feedback.transform = CGAffineTransform(rotationAngle: CGFloat.pi / -10)
-                if(q1SelectedOpt == LanguageManager.shared.currentLanguage.q1CorrectAnswer){
-                    self.q1Feedback.text = LanguageManager.shared.currentLanguage.correctAnswer
-                    self.q1Feedback.textColor =  UIColor.green
-                }else{
-                    self.q1Feedback.text = LanguageManager.shared.currentLanguage.incorrectAnswer
-                    self.q1Feedback.textColor =  UIColor.red
-                }
+            DispatchQueue.main.async {
+                self.handleQ1()
+                self.handleQ2()
                 
                 
                 if(LanguageManager.shared.currentInjury != Injury.arrest){
